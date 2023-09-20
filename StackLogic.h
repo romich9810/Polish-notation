990c6054908371b278c6108793d23e483efd8a06
Roman Chursin <105269016+romich9810@users.noreply.github.com>
2023-09-21 00:02:16 +0700

    База

---
+++ StackLogic.h
@@ -0,0 +1,22 @@
+#pragma once
+
+typedef struct stack {
+	float number;
+	char symbol;
+};
+
+stack* create();
+stack* deleteElement();
+void clearElemets(stack* stack);
+
+stack* create() {
+
+}
+
+stack* deleteElement() {
+
+}
+
+void clearElemets(stack* stack) {
+
+}
--- main.c
+++ main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+#pragma execution_character_set("utf-8")
 
+#define MAX_EXPRESSION 1000
+
+char* parsering(char* string);
 
 int main() {
-	printf("’ÛÈ\n");
+	//char* expression [];
+	//char symbol;
+
+	//while (symbol != '\n') {
+	//	realloc(expression, sizeof(&expression) + 1);
+	//}
+
+	//printf("%s", expression);
+
 	return 0;
 }
+
+char* writting() {
+	char* expression[MAX_EXPRESSION];
+	char symbol;
+
+	while (symbol != '\n') {
+		for(char* p = expression; p - expression < MAX_EXPRESSION; p++)
+		scanf("%c", expression);
+		expression++;
+	}
+
+}
+
+//char* parsering(char* string) {
+//	return NULL;
+//}
